6604d35d33561c7655e81bcb24c59a81131472ae
weishaodaren <weishaodaren@gmail.com>
2021-03-22 17:21:24 +0800

    add gragraphql

--- my-gatsby-site/src/pages/content/index.js
+++ my-gatsby-site/src/pages/content/index.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import {} from 'gatsby';
+import { graphql, useStaticQuery } from 'gatsby';
+import Layout from '../../components/layout';
 
-const Content = () => (
-  <h2>
-    <>Go to Content</>
-  </h2>
-);
+const Content = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          title
+        }
+      }
+    }
+  `);
+  return (
+    <Layout>
+      <>Go to Content</>
+      <h5>About {data.site.siteMetadata.title}</h5>
+    </Layout>
+  );
+};
+
+// export const query = graphql`
+//   query {
+//     site {
+//       siteMetadata {
+//         title
+//       }
+//     }
+//   }
+// `;
 
 export default Content;
